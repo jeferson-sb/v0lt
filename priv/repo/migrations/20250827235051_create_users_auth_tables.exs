9e5c85cb2f03e3e5036aaf313a8d31491108e29c
Jeferson Brito <jefersonsilva4k@hotmail.com>
2026-05-11 18:16:42 -0300

    update migrations

--- priv/repo/migrations/20250827235051_create_users_auth_tables.exs
+++ priv/repo/migrations/20250827235051_create_users_auth_tables.exs
@@ -15,7 +15,7 @@ defmodule Volt.Repo.Migrations.CreateUsersAuthTables do
 
     create table(:users_tokens) do
       add :user_id, references(:users, on_delete: :delete_all), null: false
-      add :token, :binary, null: false, size: 32
+      add :token, :binary, null: false
       add :context, :string, null: false
       add :sent_to, :string
 
